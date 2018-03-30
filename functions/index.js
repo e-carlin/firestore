77e71d7a3b3f98e9f2f86c4356ccd472b2345bb7
Evan Carlin <evan.carlin@slalom.com>
2018-03-30 14:45:07 -0700

    codified all indexes, using userId as sessionId

--- functions/index.js
+++ functions/index.js
@@ -7,74 +7,83 @@ admin.initializeApp(functions.config().firebase);
 var FieldValue = admin.firestore.FieldValue;
 var db = admin.firestore();
 var firestoreChatReply = {
-    userId : "",
-    message : "",
-    createdAt : ""
+    userId: "",
+    message: "",
+    messageIsFromUser: "",
+    botHasReplied: "",
+    createdAt: ""
 };
 
 //Dialogflow
 const sessionClient = new dialogflow.SessionsClient();
 const projectId = 'banter-81a54'; //https://dialogflow.com/docs/agents#settings
-const sessionId = 'quickstart-session-id';
 const languageCode = 'en-US';
 
 // Define session path
-const sessionPath = sessionClient.sessionPath(projectId, sessionId);
 
 exports.chatAdded = functions.firestore
     .document('chats/{chat}')
     .onCreate(event => {
         const chat = event.data.data();
 
-        if(chat['messageIsFromUser']) {
+        if (chat['messageIsFromUser'] && !chat['botHasReplied']) {
+            const sessionId = chat['userId'];
+            const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
             console.log('messageIsFromUser');
-        console.log('CHAT IS: ' + JSON.stringify(chat));
-        console.log('Message is: ' + chat['message']);
+            console.log('CHAT IS: ' + JSON.stringify(chat));
+            console.log('Message is: ' + chat['message']);
 
-        // The text query request.
-        var request = {
-            session: sessionPath,
-            queryInput: {
-                text: {
-                    text: chat['message'],
-                    languageCode: languageCode,
+            // The text query request.
+            var request = {
+                session: sessionPath,
+                queryInput: {
+                    text: {
+                        text: chat['message'],
+                        languageCode: languageCode,
+                    },
                 },
-            },
-        };
+            };
 
-        // Send request and log result
-        return sessionClient
-            .detectIntent(request)
-            .then(responses => {
-                console.log('RESPONSES: ' + JSON.stringify(responses));
-                console.log('Detected intent');
-                const result = responses[0].queryResult;
-                console.log(`  Query: ${result.queryText}`);
-                console.log(`  Response: ${result.fulfillmentText}`);
+            // Send request and log result
+            return sessionClient
+                .detectIntent(request)
+                .then(responses => {
+                    console.log('RESPONSES: ' + JSON.stringify(responses));
+                    console.log('Detected intent');
+                    const result = responses[0].queryResult;
+                    console.log(`  Query: ${result.queryText}`);
+                    console.log(`  Response: ${result.fulfillmentText}`);
+                    if (!result.fulfillmentText) {
+                        throw new Error("The fulfillment text was empty")
+                    }
 
 
-                return result.fulfillmentText;
-            })
-            .then((fulfillmentText) => {
-                firestoreChatReply.userId = chat['userId'];
-                firestoreChatReply.message = fulfillmentText;
-                firestoreChatReply.createdAt = FieldValue.serverTimestamp();
+                    firestoreChatReply.userId = chat['userId'];
+                    firestoreChatReply.message = result.fulfillmentText;
+                    firestoreChatReply.messageIsFromUser = false;
+                    firestoreChatReply.botHasReplied = false;
+                    firestoreChatReply.createdAt = FieldValue.serverTimestamp();
 
-                console.log('Bot response is: '+JSON.stringify(firestoreChatReply));
-                return db.collection('chats').add(firestoreChatReply)
-            })
-            .then((addedChat) => {
-                console.log('ADDED Chat: '+JSON.stringify(addedChat));
-                return "processed chat";
-            })
-            .catch(err => {
-                console.error('ERROR:', err);
-                firestoreChatReply.userId = chat['userId'];
-                firestoreChatReply.message = 'Error'+JSON.stringify(err);
-                firestoreChatReply.createdAt = FieldValue.serverTimestamp();
-                db.collection('chats').add(firestoreChatReply)
-                return "Error processing chat";
-            });
+                    console.log('Bot response is: ' + JSON.stringify(firestoreChatReply));
+                    firestoreChatReplyPromise = db.collection('chats').add(firestoreChatReply)
+
+                    userChatRefUpdatePromise = event.data.ref.update({ botHasReplied: true });
+
+                    return Promise.all([firestoreChatReplyPromise, userChatRefUpdatePromise]);
+                })
+                .then(([chatReply, userChatUpdate]) => {
+                    console.log('"************ DONE **************');
+                    return "DONE";
+                })
+                .catch(err => {
+                    console.error('ERROR:', err);
+                    firestoreChatReply.userId = chat['userId'];
+                    firestoreChatReply.message = 'Error' + JSON.stringify(err);
+                    firestoreChatReply.createdAt = FieldValue.serverTimestamp();
+                    db.collection('chats').add(firestoreChatReply)
+                    return "Error processing chat";
+                });
         }
         else {
             console.log('!messageIsFromUser');
